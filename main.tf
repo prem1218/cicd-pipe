d7606e2e9b012ca9f262c1d002371fd4531b4caa
prem1218 <prem1218@gmail.com>
2021-12-25 14:50:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "vm" {
-  ami           = "ami-061ac2e015473fbe2"
+  ami           = "ami-04505e74c0741db8d"
   #subnet_id     = "subnet-0cdddedbf69e12f61"
   instance_type = "t2.micro"
   tags = {
