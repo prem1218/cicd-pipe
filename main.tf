7873928e35c63586c35c818aff41c628dae862f0
prem1218 <prem1218@gmail.com>
2021-12-25 14:47:15 +0530

    Create main.tf
    new file

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "vm" {
+  ami           = "ami-061ac2e015473fbe2"
+  subnet_id     = "subnet-0cdddedbf69e12f61"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "my-first-tf-node-prem"
+  }
+}
